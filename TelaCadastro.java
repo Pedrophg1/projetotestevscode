3f15d0775b9d1f5583800868616c187ce1e1b484
Pedrophg1 <130565668+Pedrophg1@users.noreply.github.com>
2023-04-24 08:25:22 -0300

    mudança para o gridlayout
    a mudança foi feita para alinhar elementos a esquerda da tela

--- TelaCadastro.java
+++ TelaCadastro.java
@@ -1,14 +1,7 @@
 package votacao;
+
 import javax.swing.*;
 import java.awt.*;
-import javax.swing.JButton;
-import javax.swing.JCheckBox;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
-
 
 public class TelaCadastro implements Runnable {
 
@@ -17,57 +10,24 @@ public class TelaCadastro implements Runnable {
         telaCadastro.setSize(300, 200);
         telaCadastro.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JTextField entradaNome = new JTextField("");
         JLabel numberUser = new JLabel("Número:");
+        JTextField entradaNome = new JTextField("");
         JLabel nameUser = new JLabel("Nome:");
-        JTextField entradaName = new JTextField("");
-        entradaName.setPreferredSize(new Dimension(200, 20));
-        JLabel partidoUser = new JLabel("Partido:",  JLabel.LEFT  );
+        JLabel partidoUser = new JLabel("Partido:");
         JButton botaoEnviar = new JButton("Enviar");
 
-        JPanel painelCadastro = new JPanel(new GridBagLayout());
-        GridBagConstraints c = new GridBagConstraints();
-        c.insets = new Insets(5, 5, 5, 5); // espaçamento entre componentes
+        JPanel painelCadastro = new JPanel(new GridLayout(0, 2, 5, 5));
+        painelCadastro.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
 
-        c.gridx = 0;
-        c.gridy = 0;
-        painelCadastro.add(numberUser, c);
-
-        c.gridx = 1;
-        c.gridy = 0;
-        c.fill = GridBagConstraints.HORIZONTAL; // preencher horizontalmente
-        c.weightx = 1; // expandir horizontalmente
-       
-        entradaNome.setPreferredSize(new Dimension(200, 20)); // definir tamanho preferido
-        painelCadastro.add(entradaNome, c);
-
-        c.gridx = 0;
-        c.gridy = 1;
-        painelCadastro.add(nameUser, c);
-
-        c.gridx = 1;
-        c.gridy = 1;
-        c.fill = GridBagConstraints.HORIZONTAL; // preencher horizontalmente
-        c.weightx = 0; // expandir horizontalmente
-        
-        
-        painelCadastro.add(entradaName, c);
-         c.gridx = 1;
-        c.gridy = 3;
-        c.fill = GridBagConstraints.VERTICAL;
-        c.weighty = 1;
+        painelCadastro.add(numberUser);
+        painelCadastro.add(entradaNome);
+        painelCadastro.add(nameUser);
+        painelCadastro.add(new JPanel()); // espaço em branco
+        painelCadastro.add(new JPanel()); // espaço em branco
         painelCadastro.add(partidoUser);
-        c.gridx = 0;
-        c.gridy = 0;
-        c.fill = GridBagConstraints.HORIZONTAL; // preencher horizontalmente
-        c.weightx = 0; // expandir horizontalmente
-        
-        painelCadastro.add(Box.createVerticalGlue());
-        c.gridx = 1;
-        c.gridy = 2;
-        c.fill = GridBagConstraints.HORIZONTAL;
-        c.weighty = 1;
-        painelCadastro.add(botaoEnviar,c);
+        painelCadastro.add(new JPanel()); // espaço em branco
+        painelCadastro.add(new JPanel()); // espaço em branco
+        painelCadastro.add(botaoEnviar);
 
         telaCadastro.add(painelCadastro);
         telaCadastro.setVisible(true);
