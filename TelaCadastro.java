398bbfdcaec40ad5535a65838eb2868cc60dcced
Pedrophg1 <130565668+Pedrophg1@users.noreply.github.com>
2023-04-24 07:23:37 -0300

    mudanças na tela de voto/cadastro

--- TelaCadastro.java
+++ TelaCadastro.java
@@ -10,46 +10,70 @@ import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 
+public class TelaCadastro implements Runnable {
 
-interface TelaCadastro{
-    
+    public void run() {
+        JFrame telaCadastro = new JFrame("Tela de voto");
+        telaCadastro.setSize(300, 200);
+        telaCadastro.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        JTextField entradaNome = new JTextField("");
+        JLabel numberUser = new JLabel("Número:");
+        JLabel nameUser = new JLabel("Nome:");
+        JTextField entradaName = new JTextField("");
+        entradaName.setPreferredSize(new Dimension(200, 20));
+        JLabel partidoUser = new JLabel("Partido:",  JLabel.LEFT  );
+        JButton botaoEnviar = new JButton("Enviar");
+
+        JPanel painelCadastro = new JPanel(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+        c.insets = new Insets(5, 5, 5, 5); // espaçamento entre componentes
+
+        c.gridx = 0;
+        c.gridy = 0;
+        painelCadastro.add(numberUser, c);
+
+        c.gridx = 1;
+        c.gridy = 0;
+        c.fill = GridBagConstraints.HORIZONTAL; // preencher horizontalmente
+        c.weightx = 1; // expandir horizontalmente
+       
+        entradaNome.setPreferredSize(new Dimension(200, 20)); // definir tamanho preferido
+        painelCadastro.add(entradaNome, c);
+
+        c.gridx = 0;
+        c.gridy = 1;
+        painelCadastro.add(nameUser, c);
+
+        c.gridx = 1;
+        c.gridy = 1;
+        c.fill = GridBagConstraints.HORIZONTAL; // preencher horizontalmente
+        c.weightx = 0; // expandir horizontalmente
+        
+        
+        painelCadastro.add(entradaName, c);
+         c.gridx = 1;
+        c.gridy = 3;
+        c.fill = GridBagConstraints.VERTICAL;
+        c.weighty = 1;
+        painelCadastro.add(partidoUser);
+        c.gridx = 0;
+        c.gridy = 0;
+        c.fill = GridBagConstraints.HORIZONTAL; // preencher horizontalmente
+        c.weightx = 0; // expandir horizontalmente
+        
+        painelCadastro.add(Box.createVerticalGlue());
+        c.gridx = 1;
+        c.gridy = 2;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.weighty = 1;
+        painelCadastro.add(botaoEnviar,c);
+
+        telaCadastro.add(painelCadastro);
+        telaCadastro.setVisible(true);
+    }
+
     public static void main(String[] args) {
-        JFrame telaCadastro= new JFrame("Tela de cadastro");
-         telaCadastro.setSize(250,150);
-         telaCadastro.setBackground(Color.RED);
-        JTextField entradaNome=new JTextField("  ");
-        entradaNome.setSize(100 ,50);
-        entradaNome.setPreferredSize(new Dimension(100, 20));
-        JLabel nomeUser=new JLabel("nome:");
-        JPasswordField senhaUser= new JPasswordField("");
-        JLabel senhadoUser=new JLabel("senha:");
-        senhaUser.setPreferredSize(new Dimension(100, 20));
-        JButton botaoEnviar=new JButton("enviar");
-           
-        
-        
-        
-         JPanel painelCadastro= new JPanel();
-         
-         painelCadastro.setBackground(Color.gray);
-         painelCadastro.add(nomeUser);
-         painelCadastro.add(entradaNome);
-         painelCadastro.add(senhadoUser);
-         painelCadastro.add(senhaUser);
-         painelCadastro.add(botaoEnviar);
-         telaCadastro.add(painelCadastro);
-            telaCadastro.setVisible(true);
-
-    
-    
-    
-    
-    
-    
-    } 
-        
-    
-        
-    
-    
+        SwingUtilities.invokeLater(new TelaCadastro());
+    }
 }
